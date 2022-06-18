69d8a7adad2d5a4677012ac6cb18eed9b9817656
Asif <mdasifaahad9650@gmail.com>
2022-06-18 16:26:09 +0530

    added http and collection services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CollectionsComponent } from './components/collections/collections/collections.component';
+import { CollectionsComponent } from './components/collections/collections.component';
 
 @NgModule({
   declarations: [
